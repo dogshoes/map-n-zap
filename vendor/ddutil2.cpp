185ee0dc2fa9f518dbe7647f4a3f5f35c6ae996f
John Ehringer <jhe@5khz.com>
2014-10-22 23:40:42 -0500

    Fixing up GPL / copyright header on ddutil files from Microsoft Corporation.

--- vendor/ddutil2.cpp
+++ vendor/ddutil2.cpp
@@ -1,35 +1,16 @@
- /**********************************************************************
-    Map-N-Zap v2.x. Interface to the CYE Personal Robot
-    Copyright (C) 2000, Probotics, Inc. 
-  
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-  
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.Furthermore, this source code
-    is distributed without any promise, implied or explicit, of 
-    support from Probotics, Inc.
-  
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  
-    02111-1307, USA.
-  
-    Probotics, Inc
-    http://www.personalrobots.com
-    700 River Ave #223
-    Pittsburgh, PA 15212
-    USA
-  **********************************************************************/
+/*==========================================================================
+ *
+ *  Copyright (C) 1995-1997 Microsoft Corporation. All Rights Reserved.
+ *
+ *  File:       ddutil.cpp
+ *  Content:    Routines for loading bitmap and palettes from resources
+ *
+ ***************************************************************************/
   #undef WIN32_LEAN_AND_MEAN
   #define WIN32_LEAN_AND_MEAN
   #include <windows.h>
   #include <windowsx.h>
-  #include <ddraw.h>
+  #include "ddraw.h"
   #include "ddutil2.h"
   
   /*
