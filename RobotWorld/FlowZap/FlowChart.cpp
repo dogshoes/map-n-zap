96d506a9869eb8476f658234f16f35531ef8c3b6
John Ehringer <jhe@5khz.com>
2014-10-25 15:55:09 -0500

    There's no longer a need to call CWinApp::Enable3dControls after MFC 5.0 (VS2003 and later).

--- RobotWorld/FlowZap/FlowChart.cpp
+++ RobotWorld/FlowZap/FlowChart.cpp
@@ -69,12 +69,6 @@ BOOL CFlowChartApp::InitInstance()
     //  of your final executable, you should remove from the following
     //  the specific initialization routines you do not need.
 
-#ifdef _AFXDLL
-    Enable3dControls();			// Call this when using MFC in a shared DLL
-#else
-    Enable3dControlsStatic();	// Call this when linking to MFC statically
-#endif
-
     CFlowChartDlg dlg;
     m_pMainWnd = &dlg;
     int nResponse = dlg.DoModal();
